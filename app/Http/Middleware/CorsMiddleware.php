b5bb2748acf99c42f5ceb6c72bad45a1c9994796
unknown <you@example.com>
2023-10-10 20:39:03 +0200

    modificación CorsMiddleware

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -4,17 +4,24 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class CorsMiddleware
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next)
     {
-        return $next($request);
+        // Configura las cabeceras de respuesta para permitir CORS
+        $response = $next($request)
+            ->header("Access-Control-Allow-Origin", "http://urlfronted.example")
+            ->header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+            ->header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, X-Token-Auth, Authorization");
+
+        return $response;
     }
 }
